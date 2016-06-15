50c924ad8b073dcce8113633e8a1b11d4f89601d
Martin Carrera <mcarrera.92@gmail.com>
2016-06-15 12:19:11 -0300

    Add angular material

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -8,12 +8,30 @@
 
   function MainController(MainService) {
     var vm = this;
+    vm.options = {};
     MainService.getCards().then(function(cards){
       vm.cards = cards.data;
+      vm.generateRandomDeck();
     });
     MainService.getArenas().then(function(arenas){
       vm.arenas = arenas.data;
     });
+
+    vm.generateRandomDeck = function(){
+      vm.randomDeck = vm.shuffle(vm.cards).slice(0, 8);
+    }
+
+    vm.shuffle = function(array) {
+      var temporaryValue, randomIndex, currentIndex = array.length;
+      while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+      return array;
+    }
   }
 
 })();
